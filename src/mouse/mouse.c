2aa5b42341e3aca6947ee2b1023bfa7de8d63040
TrueBrain <truebrain@opendune.org>
2009-09-17 19:57:34 +0000

    -Add: more work on the mouse handler

--- src/mouse/mouse.c
+++ src/mouse/mouse.c
@@ -42,13 +42,11 @@ void Mouse_ExitHandler()
 	emu_pop(&emu_cx.x);
 	emu_pop(&emu_bx.x);
 	emu_pop(&emu_ax.x);
-	emu_movw(&emu_sp, emu_bp);
+	emu_sp = emu_bp;
 	emu_pop(&emu_bp);
 
-	/* Return from this function */
 	emu_pop(&emu_ip);
 	emu_pop(&emu_cs);
-	return;
 }
 
 /**
@@ -79,6 +77,173 @@ uint16 Mouse_CheckButtons(uint8 newState)
 
 	/* This should never be reached */
 	assert(!"Invalid mouse button state");
+}
+
+/**
+ * Handle the pressing and releasing of mouse buttons.
+ */
+void Mouse_HandleButtons()
+{
+	emu_ax.x = Mouse_CheckButtons(emu_get_memory16(emu_ss, emu_bp, -0x4));
+
+	emu_push(emu_ax.x);
+	emu_push(emu_cs); emu_push(0x00B9); emu_cs = 0x29E8; f__29E8_0A4A_0040_5428();
+	emu_sp += 0x2; // Remove entry from stack
+}
+
+
+/**
+ * Check if the mouse moved, and update the cursor if needed. This includes
+ *  a check if it is outside the region defined.
+ *
+ * @name Mouse_CheckMovement
+ * @implements 29A3:013F:002A:CFEE ()
+ * @implements 29A3:0152:0017:5360
+ * @implements 29A3:0169:0008:63C3
+ * @implements 29A3:0171:0004:C0E6
+ * @implements 29A3:0175:0028:0C94
+ * @implements 29A3:019D:000C:9EC2
+ * @implements 29A3:01A9:0008:63C5
+ * @implements 29A3:01B1:0004:C0E6
+ * @implements 29A3:01B5:001A:784E
+ */
+void Mouse_CheckMovement()
+{
+	/* XXX -- It avoids mouse movement .. but what is it? */
+	if (emu_get_memory16(emu_ds, 0x00,  0x706A) != 0x0) return;
+
+	/* Did the mouse move? */
+	if (emu_get_memory16(emu_ds, 0x00,  0x707C) == emu_cx.x && emu_get_memory16(emu_ds, 0x00,  0x707E) == emu_dx.x) return;
+
+	if ((emu_get_memory16(emu_ds, 0x00,  0x7080) & 0xC000) != 0xC000) {
+		/* XXX -- Parameters for the next function */
+		emu_push(emu_get_memory16(emu_ds, 0x00,  0x7094));
+		emu_push(emu_get_memory16(emu_ds, 0x00,  0x7092));
+
+		/* Call/jump based on memory/register values */
+		emu_ip = emu_get_memory16(emu_ds, 0x00,  0x66B4);
+		emu_push(emu_cs);
+		emu_cs = emu_get_memory16(emu_ds, 0x00,  0x66B6);
+		emu_push(0x0169);
+		switch ((emu_cs << 16) + emu_ip) {
+			case 0x22A60FD7: f__22A6_0FD7_0043_ACF9(); break;
+			default:
+				/* In case we don't know the call point yet, call the dynamic call */
+				emu_last_cs = 0x29A3; emu_last_ip = 0x0165; emu_last_length = 0x002A; emu_last_crc = 0xCFEE;
+				emu_call();
+				return;
+		}
+		emu_sp += 0x4; // Release the two stack values
+
+		emu_push(emu_cs); emu_push(0x0171); emu_cs = 0x2B6C; f__2B6C_000E_0045_C1FE();
+
+		/* Call/jump based on memory/register values */
+		emu_ip = emu_get_memory16(emu_ds, 0x00,  0x66A4);
+		emu_push(emu_cs);
+		emu_cs = emu_get_memory16(emu_ds, 0x00,  0x66A6);
+		emu_push(0x0175);
+		switch ((emu_cs << 16) + emu_ip) {
+			case 0x22A60FAE: f__22A6_0FAE_0027_2378(); break;
+			default:
+				/* In case we don't know the call point yet, call the dynamic call */
+				emu_last_cs = 0x29A3; emu_last_ip = 0x0171; emu_last_length = 0x0004; emu_last_crc = 0xC0E6;
+				emu_call();
+				return;
+		}
+	}
+
+	/* Check if the cursor is still inside a certain region */
+	if ((emu_get_memory16(emu_ds, 0x00,  0x7080) & 0x8000) == 0x8000) {
+		if (emu_cx.x >= emu_get_memory16(emu_ds, 0x00,  0x7082) ||
+		    emu_cx.x <= emu_get_memory16(emu_ds, 0x00,  0x7086) ||
+		    emu_dx.x >= emu_get_memory16(emu_ds, 0x00,  0x7084) ||
+		    emu_dx.x <= emu_get_memory16(emu_ds, 0x00,  0x7088)) {
+			emu_get_memory16(emu_ds, 0x00,  0x7080) |= 0x4000;
+			return;
+		}
+	}
+
+	/* XXX -- Parameters for the next function */
+	emu_push(emu_get_memory16(emu_ds, 0x00,  0x7094));
+	emu_push(emu_get_memory16(emu_ds, 0x00,  0x7092));
+
+	/* Call/jump based on memory/register values */
+	emu_ip = emu_get_memory16(emu_ds, 0x00,  0x66B4);
+	emu_push(emu_cs);
+	emu_cs = emu_get_memory16(emu_ds, 0x00,  0x66B6);
+	emu_push(0x01A9);
+	switch ((emu_cs << 16) + emu_ip) {
+		case 0x22A60FD7: f__22A6_0FD7_0043_ACF9(); break;
+		default:
+			/* In case we don't know the call point yet, call the dynamic call */
+			emu_last_cs = 0x29A3; emu_last_ip = 0x01A5; emu_last_length = 0x000C; emu_last_crc = 0x9EC2;
+			emu_call();
+			return;
+	}
+	emu_sp += 0x4; // Release the two stack values
+
+	emu_push(emu_cs); emu_push(0x01B1); emu_cs = 0x2B6C; f__2B6C_006E_002E_4FBC();
+
+	/* Call/jump based on memory/register values */
+	emu_ip = emu_get_memory16(emu_ds, 0x00,  0x66A4);
+	emu_push(emu_cs);
+	emu_cs = emu_get_memory16(emu_ds, 0x00,  0x66A6);
+	emu_push(0x01B5);
+	switch ((emu_cs << 16) + emu_ip) {
+		case 0x22A60FAE: f__22A6_0FAE_0027_2378(); break;
+		default:
+			/* In case we don't know the call point yet, call the dynamic call */
+			emu_last_cs = 0x29A3; emu_last_ip = 0x01B1; emu_last_length = 0x0004; emu_last_crc = 0xC0E6;
+			emu_call();
+			return;
+	}
+
+	return;
+}
+
+/**
+ * Handle the movement of the mouse.
+ *
+ * @name Mouse_HandleMovement
+ * @implements 29A3:00CD:0066:C700 ()
+ * @implements 29A3:00F3:0040:B81C
+ * @implements 29A3:010F:0024:C7FA
+ */
+void Mouse_HandleMovement()
+{
+	emu_get_memory16(emu_ds, 0x00,  0x705E) = 0x1;
+
+	/* Snap to grid on x-axis */
+	if (emu_get_memory16(emu_ds, 0x00,  0x6C78) != 0x0) {
+		uint16 snap = emu_get_memory16(emu_ds, 0x00,  0x6C78);
+
+		emu_cx.x -= emu_get_memory16(emu_ds, 0x00,  0x6C7C);
+		emu_cx.x = (emu_cx.x / snap) * snap;
+		emu_cx.x += emu_get_memory16(emu_ds, 0x00,  0x6C7C);
+	}
+
+	/* Snap to grid on y-axis */
+	if (emu_get_memory16(emu_ds, 0x00,  0x6C7A) != 0x0) {
+		uint16 snap = emu_get_memory16(emu_ds, 0x00,  0x6C7A);
+
+		emu_dx.x -= emu_get_memory16(emu_ds, 0x00,  0x6C7E);
+		emu_dx.x = (emu_dx.x / snap) * snap;
+		emu_dx.x += emu_get_memory16(emu_ds, 0x00,  0x6C7E);
+	}
+
+	emu_get_memory16(emu_ds, 0x00,  0x7060) = emu_cx.x;
+	emu_get_memory16(emu_ds, 0x00,  0x7062) = emu_dx.x;
+
+	if (emu_get_memory8(emu_ds, 0x00,  0x7010) == 0x01 && (emu_get_memory16(emu_ds, 0x00,  0x700E) & 0x1000) == 0) {
+		Mouse_HandleButtons();
+	}
+
+	Mouse_CheckMovement();
+
+	emu_get_memory16(emu_ds, 0x00,  0x707C) = emu_cx.x;
+	emu_get_memory16(emu_ds, 0x00,  0x707E) = emu_dx.x;
+	emu_get_memory16(emu_ds, 0x00,  0x705E) = 0x0;
+	Mouse_ExitHandler();
 }
 
 /**
@@ -124,18 +289,13 @@ void Mouse_EventHandler()
 
 	if (emu_cx.x > 319) emu_cx.x = 319;
 
-	/* XXX -- More checks. I guess it tells if the mouse is visible and if it can interact */
 	if (emu_get_memory8(emu_ds, 0x00,  0x7010) == 0x00 && (emu_get_memory16(emu_ds, 0x00,  0x700E) & 0x1000) == 0) {
-		emu_ax.x = Mouse_CheckButtons(emu_get_memory16(emu_ss, emu_bp, -0x4));
-
-		emu_push(emu_ax.x);
-		emu_push(emu_cs); emu_push(0x00B9); emu_cs = 0x29E8; f__29E8_0A4A_0040_5428();
-		emu_sp += 0x2; // Remove entry from stack
+		Mouse_HandleButtons();
 	}
 
 	/* XXX -- More checks */
 	if (emu_get_memory8(emu_ds, 0x00,  0x7010) == 0x02) { Mouse_ExitHandler(); return; }
 	if (emu_get_memory16(emu_ds, 0x00,  0x705E) != 0x0000) { Mouse_ExitHandler(); return; }
 
-	f__29A3_00CD_0066_C700();
+	Mouse_HandleMovement();
 }
--- src/mouse/mouse.h
+++ src/mouse/mouse.h
@@ -4,13 +4,46 @@
 /*
  * Mouse Memory:
  *
- * 353F:700E -> If no mask 0x1000, no mouse click?
- * 353F:7010 -> Mouse mode?
- * 353F:705E -> If non-zero, only button handling (mouse pointer not visible?)
- * 353F:7068 -> If one, mouse reports 640 in width
- * 353F:7097 -> If non-zero, no mouse handling
- * 353F:7098 -> If zero, no mouse handling
- * 353F:7099 -> Last mouse button state
+ * 353F:
+ *      66A4(4) -> cs:ip of routine called after mouse redraw
+ *
+ *      66B4(4) -> cs:ip of routine called before mouse redraw
+ *
+ *      6C78(2) -> Snap mouse to grid, x-axis
+ *      6C7A(2) -> Snap mouse to grid, y-axis
+ *      6C7C(2) -> Snap grey-zone on x-axis
+ *      6C7E(2) -> Snap grey-zone on y-axis
+ *
+ *      700E(2) -> ?? If no mask 0x1000, no mouse click handling
+ *      7010(1) -> Mouse mode
+ *                   0 - Normal mouse mode
+ *                   1 - ??
+ *                   2 - Only button clicks
+ *
+ *      705E(2) -> Lock variable for when moving mouse
+ *
+ *      7060(2) -> New X position
+ *      7062(2) -> New Y position
+ *
+ *      7068(2) -> ?? If one, mouse reports 640 in width
+ *      706A(2) -> ?? If non-zero, mouse movement is not registered
+ *
+ *      707C(2) -> Last X position
+ *      707E(2) -> Last Y position
+ *      7080(2) -> Bitfield:
+ *                   0x4000 - Mouse still inside region
+ *                   0x8000 - Region check
+ *      7082(2) -> Region - minimum value for X position
+ *      7084(2) -> Region - minimum value for Y position
+ *      7086(2) -> Region - maximum value for X position
+ *      7088(2) -> Region - maximum value for Y position
+ *
+ *      7092(2) -> Parameter 1 for proc at 66B4
+ *      7094(2) -> Paramerer 2 for proc at 66B4
+ *
+ *      7097(1) -> ?? If non-zero, no mouse handling
+ *      7098(1) -> ?? If zero, no mouse handling
+ *      7099(1) -> Last mouse button state
  */
 
 extern void Mouse_EventHandler();
