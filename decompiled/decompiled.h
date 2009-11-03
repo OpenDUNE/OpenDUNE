da626fccf5cee40d6cd26b43ddaa3583d649a9f5
TrueBrain <truebrain@opendune.org>
2009-11-03 13:34:03 +0000

    -Fix (r446): I fucked up ;)

--- decompiled/decompiled.h
+++ decompiled/decompiled.h
@@ -11,7 +11,6 @@
 #include "../src/pool/house.h"
 #include "../src/global.h"
 #include "../src/building.h"
-#include "../src/scenario.h"
 #include "../src/input/input.h"
 #include "../src/unit.h"
 #include "../src/tools.h"
@@ -50,6 +49,7 @@ extern void emu_EntryPoint();
 extern void f__B4B1_0000_0016_067A();
 extern void f__B50B_0000_0029_BDBF();
 extern void f__2BF2_0000_0007_42B5();
+extern void emu_Scenario_Load();
 extern void f__B4B8_0000_001F_3BC3();
 extern void f__2598_0000_0017_EB80();
 extern void f__B511_0000_000E_B463();
