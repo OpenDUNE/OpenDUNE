02ceb38fb58230795c72fb971ea52d604582f65d
TrueBrain <truebrain@opendune.org>
2009-11-04 09:49:14 +0000

    -Add: an 'os' layer for non-C89 compatibility (like strcasecmp)

---
+++ src/os/strings.h
@@ -0,0 +1,17 @@
+#ifndef OS_STRINGS_H
+#define OS_STRINGS_H
+
+/*
+ * This header should define the following functions:
+ *
+ * strcasecmp - strcmp, but case insensitive
+ *
+ */
+
+#include <strings.h>
+
+#if defined(MSVC)
+	#define stricmp strcasecmp
+#endif /* MSVC */
+
+#endif /* OS_STRINGS_H */
--- src/unit.c
+++ src/unit.c
@@ -1,7 +1,7 @@
 /* $Id$ */
 
 #include <stdio.h>
-#include <strings.h>
+#include "os/strings.h"
 #include "types.h"
 #include "libemu.h"
 #include "global.h"
