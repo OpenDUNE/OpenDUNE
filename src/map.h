b1b9665edade5c4ddb4ec4f9e1eeaa7140e6971f
TrueBrain <truebrain@opendune.org>
2009-12-08 14:13:43 +0000

    -Add: the understanding of one more flag

--- src/map.h
+++ src/map.h
@@ -11,8 +11,8 @@ typedef struct Tile {
 	PACK uint32 spriteID:9;                                 /*!< The Sprite which is drawn on this Tile. */
 	PACK uint32 fogOfWar:7;                                 /*!< The Fog of War on this Tile. */
 	PACK uint32 houseID:3;                                  /*!< Which House owns this Tile. */
-	PACK uint32 flags:5;                                    /*!< Bitflags. 0x01 - ??, 0x02 - ??, 0x04 - Has Structure, 0x08 - ??, 0x10 - ??. */
-	PACK uint32 index:8;                                    /*!< Index of the structure. */
+	PACK uint32 flags:5;                                    /*!< Bitflags. 0x01 - ??, 0x02 - ??, 0x04 - Has Structure, 0x08 - ??, 0x10 - Has Unit. */
+	PACK uint32 index:8;                                    /*!< Index of the Structure / Unit (index 1 is Structure/Unit 0, etc). */
 } GCC_PACKED Tile;
 MSVC_PACKED_END
 assert_compile(sizeof(Tile) == 0x04);
