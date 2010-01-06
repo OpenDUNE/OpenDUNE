ffe596f755e1e9fec027e3c731bec706b50f2823
glx <glx@opendune.org>
2010-01-06 15:12:38 +0000

    -Fix (#26): of course snprintf declaration has to be different for OSX

--- src/os/strings.h
+++ src/os/strings.h
@@ -25,7 +25,11 @@
 	#endif /* __MINGW32__ && __STRICT_ANSI__ */
 	#if !defined(__MINGW32__) && defined(__GNUC__) && !defined(snprintf)
 		/* snprintf is in fact C99, but we like to use it over sprintf for the obvious reasons */
-		extern int snprintf (char *__restrict __s, size_t __maxlen, __const char *__restrict __format, ...) __THROW __attribute__ ((__format__ (__printf__, 3, 4)));
+		#if !defined(__APPLE__)
+			extern int snprintf (char *__restrict __s, size_t __maxlen, __const char *__restrict __format, ...) __THROW __attribute__ ((__format__ (__printf__, 3, 4)));
+		#else /* __APPLE__ */
+			extern int snprintf(char * __restrict, size_t, const char * __restrict, ...) __DARWIN_LDBL_COMPAT(snprintf);
+		#endif /* __APPLE__ */
 	#endif /* __GCC__ */
 #endif /* _MSC_VER && (!DECOMPILED_INCLUDE || !__TINYC__) */
 
