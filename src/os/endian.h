a8eb8ac925baf9618ace3040373acacd74ddb850
glx <glx@opendune.org>
2010-02-02 13:27:21 +0000

    -Fix (r839): make os/endian.h MSVC and MINGW compatible

--- src/os/endian.h
+++ src/os/endian.h
@@ -3,7 +3,21 @@
 #ifndef OS_ENDIAN_H
 #define OS_ENDIAN_H
 
-#include <endian.h>
+#if defined(_WIN32)
+	#define __BYTE_ORDER __LITTLE_ENDIAN
+	#if defined(_MSC_VER)
+		static uint32 __builtin_bswap32(uint32 x)
+		{
+			__asm {
+				mov eax, x;
+				bswap eax;
+			}
+		}
+	#endif /* _MSC_VER */
+#else /* _WIN32 */
+	#include <endian.h>
+#endif /* _WIN32 */
+
 #if __BYTE_ORDER == __LITTLE_ENDIAN
 	#define HTOBE32(x) __builtin_bswap32(x)
 #else
