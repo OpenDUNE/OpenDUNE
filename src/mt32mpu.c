3df5398b4ee1cf366665620ee5b5bc9cfb199c3b
glx <glx@opendune.org>
2010-03-03 18:48:39 +0000

    -Cleanup: unneeded instructions in MPU_* functions

--- src/mt32mpu.c
+++ src/mt32mpu.c
@@ -22,9 +22,6 @@ static bool MPU_WriteCommand(uint8 command)
 	uint16 i;
 	bool ret = false;
 
-	emu_pushf();
-	emu_cli();
-
 	for (i = 0; i < 0xFFFF; i++) {
 		uint8 value;
 		emu_inb(&value, 0x331);
@@ -41,8 +38,6 @@ static bool MPU_WriteCommand(uint8 command)
 		if (value == 0xFE) ret = true;
 	}
 
-	emu_popf();
-
 	return ret;
 }
 
@@ -50,25 +45,15 @@ bool MPU_Reset()
 {
 	bool ret;
 
-	emu_pushf();
-	emu_cli();
-
 	ret = MPU_WriteCommand(0xFF);
 	if (!ret) ret = MPU_WriteCommand(0xFF);
-
-	emu_popf();
 
 	return ret;
 }
 
 void MPU_UART()
 {
-	emu_pushf();
-	emu_cli();
-
 	MPU_WriteCommand(0x3F);
-
-	emu_popf();
 }
 
 void MPU_WriteData(uint8 data)
