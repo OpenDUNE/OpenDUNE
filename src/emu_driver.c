67a3bcd27aea4e4faac0c1302bd105b76e312c36
TrueBrain <truebrain@opendune.org>
2010-03-07 00:11:30 +0000

    -Remove (r945): removed now unused emu_ functions

--- src/emu_driver.c
+++ src/emu_driver.c
@@ -9,33 +9,6 @@
 #include "global.h"
 
 /**
- * Emulator wrapper around Drivers_All_Init()
- *
- * @name emu_Drivers_All_Init
- * @implements 1DD7:039B:0008:D3BD ()
- */
-void emu_Drivers_All_Init()
-{
-	csip32 ret;
-	uint16 sound;
-	uint16 music;
-	uint16 voice;
-
-	/* Pop the return CS:IP. */
-	emu_pop(&ret.s.ip);
-	emu_pop(&ret.s.cs);
-
-	sound = emu_get_memory16(emu_ss, emu_sp, 0);
-	music = emu_get_memory16(emu_ss, emu_sp, 2);
-	voice = emu_get_memory16(emu_ss, emu_sp, 4);
-
-	Drivers_All_Init(sound, music, voice);
-
-	emu_cs = ret.s.cs;
-	emu_ip = ret.s.ip;
-}
-
-/**
  * Emulator wrapper around Drivers_GetFunctionCSIP()
  *
  * @name emu_Drivers_GetFunctionCSIP
