379f78b8792a469e9910f5c56bac4da7eec191a2
glx <glx@opendune.org>
2011-01-06 17:25:40 +0000

    -Fix (r1236): forgot an extern

--- src/map.c
+++ src/map.c
@@ -28,6 +28,7 @@ extern void f__2598_0000_0017_EB80();
 extern void f__2B6C_0137_0020_C73F();
 extern void f__2B6C_0169_001E_6939();
 extern void f__B483_0000_0019_F96A();
+extern void f__B4CD_057B_001A_D066();
 extern void f__B4CD_0AFA_0011_D5DB();
 extern void f__B4CD_14CA_0013_F579();
 extern void f__B4CD_1CDA_000C_C72C();
