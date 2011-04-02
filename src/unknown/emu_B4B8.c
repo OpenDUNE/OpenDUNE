114ae994b7486c07ad44be94e59107a29c76c205
TrueBrain <truebrain@opendune.org>
2011-04-02 22:21:00 +0000

    -Fix: Alberth was missing an unknown file

---
+++ src/unknown/emu_B4B8.c
@@ -0,0 +1,47 @@
+/* $Id$ */
+
+#include <stdio.h>
+#include "types.h"
+#include "libemu.h"
+#include "../global.h"
+#include "unknown.h"
+
+/**
+ * C-ified function f_ _B4B8_110D_000D_FD5C().
+ * @param houseID The house.
+ */
+void Unknown_B4B8_110D(uint8 houseID)
+{
+	int16 i;
+	int16 loc6;
+	uint8 *ptr;
+
+	ptr = emu_get_memorycsip(g_global->variable_3C42);
+	for (i = 0; i < 0x100; i++, ptr++) {
+		*ptr = i;
+
+		loc6 = i / 16;
+		if (loc6 == 9) {
+			*ptr = (houseID << 4) + 0x90;
+		}
+	}
+}
+
+/**
+ * Emulator wrapper around #Unknown_B4B8_110D
+ *
+ * @name emu_Unknown_B4B8_110D
+ * @implements B4B8:110D:000D:FD5C ()
+ */
+void emu_Unknown_B4B8_110D()
+{
+	uint8 houseID;
+
+	/* Pop return address from the stack. */
+	emu_pop(&emu_ip);
+	emu_pop(&emu_cs);
+
+	houseID = emu_get_memory16(emu_ss, emu_sp, 0x0);
+
+	Unknown_B4B8_110D(houseID);
+}
