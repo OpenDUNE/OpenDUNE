4d406bdef0119dd0b0e7695ddb74f0e2137703e6
glx <glx@opendune.org>
2011-06-21 15:06:46 +0000

    -Fix: MSVC no longer wants sleep.h to be included before math.h

--- src/os/math.h
+++ src/os/math.h
@@ -5,13 +5,17 @@
 #ifndef OS_MATH_H
 #define OS_MATH_H
 
-#ifndef max
-	#define max(a, b) ((a) > (b) ? (a) : (b))
-#endif /* max */
+#ifdef _MSC_VER
+	#include <stdlib.h>
+#else
+	#ifndef max
+		#define max(a, b) ((a) > (b) ? (a) : (b))
+	#endif /* max */
 
-#ifndef min
-	#define min(a, b) ((a) < (b) ? (a) : (b))
-#endif /* min */
+	#ifndef min
+		#define min(a, b) ((a) < (b) ? (a) : (b))
+	#endif /* min */
+#endif
 
 #ifndef clamp
 	#define clamp(a, b, c) (min(max(a, b), c))
