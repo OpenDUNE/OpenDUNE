bff1c66782509ad6ba7def384e11222174041d28
glx <glx@opendune.org>
2011-08-02 21:34:47 +0000

    -Cleanup: leftover declarations

--- src/dsp.h
+++ src/dsp.h
@@ -5,13 +5,10 @@
 #ifndef DSP_H
 #define DSP_H
 
-struct DriverInfo;
-
 extern void DSP_Play(uint8 *data);
 extern void DSP_Stop();
 extern uint8 DSP_GetStatus();
 extern void DSP_Init();
 extern void DSP_Uninit();
-extern struct DriverInfo *DSP_GetInfo();
 
 #endif /* DSP_H */
--- src/mt32mpu.h
+++ src/mt32mpu.h
@@ -5,8 +5,6 @@
 #ifndef MT32MPU_H
 #define MT32MPU_H
 
-struct DriverInfo;
-
 extern void MPU_Interrupt();
 extern uint16 MPU_SetData(uint8 *file, uint16 index, void *msdata);
 extern void MPU_Play(uint16 index);
@@ -16,7 +14,6 @@ extern uint16 MPU_GetDataSize();
 extern void MPU_Init();
 extern void MPU_Uninit();
 extern void MPU_ClearData(uint16 index);
-extern struct DriverInfo *MPU_GetInfo();
 extern void MPU_SetVolume(uint16 index, uint16 volume, uint16 arg0C);
 
 #endif /* MT32MPU_H */
