abb0276fbef0ef4f06bcacbfbaea0ad0864e78a7
glx <glx@opendune.org>
2011-08-15 16:04:51 +0000

    -Codechange: implement safer [v]snprintf() for MSVC

--- src/os/strings.h
+++ src/os/strings.h
@@ -15,10 +15,57 @@
  */
 
 #if defined(_MSC_VER)
+	#include <assert.h>
+	#include <errno.h>
 	#include <string.h>
+	#include <stdarg.h>
 	#define strcasecmp _stricmp
 	#define strncasecmp _strnicmp
-	#define snprintf _snprintf
+
+	static int snprintf(char *str, size_t size, const char *format, ...)
+	{
+		va_list ap;
+		int ret;
+
+		va_start(ap, format);
+		ret = vsnprintf(str, size, format, ap);
+		va_end(ap);
+		return ret;
+	}
+
+	/**
+	 * Almost POSIX compliant implementation of \c vsnprintf for VC compiler.
+	 * The difference is in the value returned on output truncation. This
+	 * implementation returns size whereas a POSIX implementation returns
+	 * size or more (the number of bytes that would be written to str
+	 * had size been sufficiently large excluding the terminating null byte).
+	 */
+	static int vsnprintf(char *str, size_t size, const char *format, va_list ap)
+	{
+		int ret;
+		if (size == 0) return 0;
+
+		errno = 0;
+		ret = _vsnprintf(str, size, format, ap);
+
+		if (ret < 0) {
+			if (errno != ERANGE) {
+				/* There's a formatting error, better get that looked
+				 * at properly instead of ignoring it. */
+				assert(0);
+			}
+		} else if ((size_t)ret < size) {
+			/* The buffer is big enough for the number of
+			 * characers stored (excluding null), i.e.
+			 * the string has been null-terminated. */
+			return ret;
+		}
+
+		/* The buffer is too small for _vsnprintf to write the
+		 * null-terminator at its end and return size. */
+		str[size - 1] = '\0';
+		return (int)size;
+	}
 #elif !defined(DECOMPILED_INCLUDE) || !defined(__TINYC__)
 	#include <strings.h>
 	#if defined(__MINGW32__) && defined(__STRICT_ANSI__)
