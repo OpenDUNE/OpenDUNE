49f73842ee9c1bd42ad1c0b0f18c70fd1d51fd2c
TrueBrain <truebrain@opendune.org>
2011-08-16 19:30:53 +0000

    -Fix (r2392): crashlog_none.c didn't compile

--- src/crashlog/crashlog_none.c
+++ src/crashlog/crashlog_none.c
@@ -4,6 +4,7 @@
 
 #if !defined(_WIN32)
 
+#include "types.h"
 #include "crashlog.h"
 
 void CrashLog_Init()
@@ -12,18 +13,22 @@ void CrashLog_Init()
 
 void CrashLog_LogError(char *buffer)
 {
+	VARIABLE_NOT_USED(buffer);
 }
 
 void CrashLog_LogRegisters(char *buffer)
 {
+	VARIABLE_NOT_USED(buffer);
 }
 
-void CrashLog_LogStackTrace(char *buffer)
+void CrashLog_LogStacktrace(char *buffer)
 {
+	VARIABLE_NOT_USED(buffer);
 }
 
 void CrashLog_LogOSVersion(char *buffer)
 {
+	VARIABLE_NOT_USED(buffer);
 }
 
 #endif /* !_WIN32 */
