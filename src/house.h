7364a2a6441b3b73d214e5f2d2acb44948353908
TrueBrain <truebrain@opendune.org>
2011-08-19 10:25:29 +0000

    -Codechange: unified comment style in House

--- src/house.h
+++ src/house.h
@@ -48,34 +48,34 @@ typedef enum HouseAnimationType {
  * A %House as stored in the memory.
  */
 typedef struct House {
-	uint8  index;                      /*!< The index of the House in the array. */
-	uint16 harvestersIncoming;         /*!< How many harvesters are waiting to be delivered. Only happens when we run out of Units to do it immediately. */
+	uint8  index;                                           /*!< The index of the House in the array. */
+	uint16 harvestersIncoming;                              /*!< How many harvesters are waiting to be delivered. Only happens when we run out of Units to do it immediately. */
 	struct {
-		BIT_U8 used:1;                 /*!< The House is in use (no longer free in the pool). */
-		BIT_U8 human:1;                /*!< The House is controlled by a human. */
-		BIT_U8 doneFullScaleAttack:1;  /*!< The House did his one time attack the human with everything we have. */
-		BIT_U8 isAIActive:1;           /*!< The House has been seen by the human, and everything now becomes active (Team attack, house missiles, rebuilding, ..). */
-		BIT_U8 radarActivated:1;       /*!< The radar is activated. */
-		BIT_U8 unused_0020:3;          /*!< Unused */
-	} flags;                           /*!< General flags of the House. */
-	uint16 unitCount;                  /*!< Amount of units owned by House. */
-	uint16 unitCountMax;               /*!< Maximum amount of units this House is allowed to have. */
-	uint16 unitCountEnemy;             /*!< ?? Amount of units owned by allied. */
-	uint16 unitCountAllied;            /*!< ?? Amount of units owned by enemy. */
-	uint32 structuresBuilt;            /*!< The Nth bit active means the Nth structure type is built (one or more). */
-	uint16 credits;                    /*!< Amount of credits the House currently has. */
-	uint16 creditsStorage;             /*!< Amount of credits the House can store. */
-	uint16 powerProduction;            /*!< Amount of power the House produces. */
-	uint16 powerUsage;                 /*!< Amount of power the House requires. */
-	uint16 windtrapCount;              /*!< Amount of windtraps the House currently has. */
-	uint16 creditsQuota;               /*!< Quota house has to reach to win the mission. */
-	tile32 palacePosition;             /*!< Position of the Palace. */
-	uint16 timerUnitAttack;            /*!< Timer to count down when next 'unit approaching' message can be showed again. */
-	uint16 timerSandwormAttack;        /*!< Timer to count down when next 'sandworm approaching' message can be showed again. */
-	uint16 timerStructureAttack;       /*!< Timer to count down when next 'base is under attack' message can be showed again. */
-	uint16 starportTimeLeft;           /*!< How much time is left before starport transport arrives. */
-	uint16 starportLinkedID;           /*!< If there is a starport delivery, this indicates the first unit of the linked list. Otherwise it is 0xFFFF. */
-	uint16 ai_structureRebuild[5][2];  /*!< An array for the AI which stores the type and position of a destroyed structure, for rebuilding. */
+		BIT_U8 used:1;                                      /*!< The House is in use (no longer free in the pool). */
+		BIT_U8 human:1;                                     /*!< The House is controlled by a human. */
+		BIT_U8 doneFullScaleAttack:1;                       /*!< The House did his one time attack the human with everything we have. */
+		BIT_U8 isAIActive:1;                                /*!< The House has been seen by the human, and everything now becomes active (Team attack, house missiles, rebuilding, ..). */
+		BIT_U8 radarActivated:1;                            /*!< The radar is activated. */
+		BIT_U8 unused_0020:3;                               /*!< Unused */
+	} flags;                                                /*!< General flags of the House. */
+	uint16 unitCount;                                       /*!< Amount of units owned by House. */
+	uint16 unitCountMax;                                    /*!< Maximum amount of units this House is allowed to have. */
+	uint16 unitCountEnemy;                                  /*!< Amount of units owned by allies. */
+	uint16 unitCountAllied;                                 /*!< Amount of units owned by enemy. */
+	uint32 structuresBuilt;                                 /*!< The Nth bit active means the Nth structure type is built (one or more). */
+	uint16 credits;                                         /*!< Amount of credits the House currently has. */
+	uint16 creditsStorage;                                  /*!< Amount of credits the House can store. */
+	uint16 powerProduction;                                 /*!< Amount of power the House produces. */
+	uint16 powerUsage;                                      /*!< Amount of power the House requires. */
+	uint16 windtrapCount;                                   /*!< Amount of windtraps the House currently has. */
+	uint16 creditsQuota;                                    /*!< Quota house has to reach to win the mission. */
+	tile32 palacePosition;                                  /*!< Position of the Palace. */
+	uint16 timerUnitAttack;                                 /*!< Timer to count down when next 'unit approaching' message can be showed again. */
+	uint16 timerSandwormAttack;                             /*!< Timer to count down when next 'sandworm approaching' message can be showed again. */
+	uint16 timerStructureAttack;                            /*!< Timer to count down when next 'base is under attack' message can be showed again. */
+	uint16 starportTimeLeft;                                /*!< How much time is left before starport transport arrives. */
+	uint16 starportLinkedID;                                /*!< If there is a starport delivery, this indicates the first unit of the linked list. Otherwise it is 0xFFFF. */
+	uint16 ai_structureRebuild[5][2];                       /*!< An array for the AI which stores the type and position of a destroyed structure, for rebuilding. */
 } House;
 
 /**
