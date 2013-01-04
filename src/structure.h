a99d4cbb17dbfc0b5c807fdfd63f0581714dc63d
TrueBrain <truebrain@opendune.org>
2013-01-04 13:10:25 +0100

    -Codechange: use enums for structuresRequired  (based on patch by wangds)
    FLAG_STRUCTURE_NEVER is a bit special, but by the lack of a better name
    it will have to do.

--- src/structure.h
+++ src/structure.h
@@ -33,6 +33,31 @@ typedef enum StructureType {
 	STRUCTURE_INVALID           = 0xFF
 } StructureType;
 
+typedef enum StructureFlag {
+	FLAG_STRUCTURE_SLAB_1x1          = 1 << STRUCTURE_SLAB_1x1,          /* 0x____01 */
+	FLAG_STRUCTURE_SLAB_2x2          = 1 << STRUCTURE_SLAB_2x2,          /* 0x____02 */
+	FLAG_STRUCTURE_PALACE            = 1 << STRUCTURE_PALACE,            /* 0x____04 */
+	FLAG_STRUCTURE_LIGHT_VEHICLE     = 1 << STRUCTURE_LIGHT_VEHICLE,     /* 0x____08 */
+	FLAG_STRUCTURE_HEAVY_VEHICLE     = 1 << STRUCTURE_HEAVY_VEHICLE,     /* 0x____10 */
+	FLAG_STRUCTURE_HIGH_TECH         = 1 << STRUCTURE_HIGH_TECH,         /* 0x____20 */
+	FLAG_STRUCTURE_HOUSE_OF_IX       = 1 << STRUCTURE_HOUSE_OF_IX,       /* 0x____40 */
+	FLAG_STRUCTURE_WOR_TROOPER       = 1 << STRUCTURE_WOR_TROOPER,       /* 0x____80 */
+	FLAG_STRUCTURE_CONSTRUCTION_YARD = 1 << STRUCTURE_CONSTRUCTION_YARD, /* 0x__01__ */
+	FLAG_STRUCTURE_WINDTRAP          = 1 << STRUCTURE_WINDTRAP,          /* 0x__02__ */
+	FLAG_STRUCTURE_BARRACKS          = 1 << STRUCTURE_BARRACKS,          /* 0x__04__ */
+	FLAG_STRUCTURE_STARPORT          = 1 << STRUCTURE_STARPORT,          /* 0x__08__ */
+	FLAG_STRUCTURE_REFINERY          = 1 << STRUCTURE_REFINERY,          /* 0x__10__ */
+	FLAG_STRUCTURE_REPAIR            = 1 << STRUCTURE_REPAIR,            /* 0x__20__ */
+	FLAG_STRUCTURE_WALL              = 1 << STRUCTURE_WALL,              /* 0x__40__ */
+	FLAG_STRUCTURE_TURRET            = 1 << STRUCTURE_TURRET,            /* 0x__80__ */
+	FLAG_STRUCTURE_ROCKET_TURRET     = 1 << STRUCTURE_ROCKET_TURRET,     /* 0x01____ */
+	FLAG_STRUCTURE_SILO              = 1 << STRUCTURE_SILO,              /* 0x02____ */
+	FLAG_STRUCTURE_OUTPOST           = 1 << STRUCTURE_OUTPOST,           /* 0x04____ */
+
+	FLAG_STRUCTURE_NONE              = 0,
+	FLAG_STRUCTURE_NEVER             = 0xFFFF                            /*!< Special flag to mark that certain buildings can never be built on a Construction Yard. */
+} StructureFlag;
+
 /** Available structure layouts. */
 typedef enum StructureLayout {
 	STRUCTURE_LAYOUT_1x1 = 0,
