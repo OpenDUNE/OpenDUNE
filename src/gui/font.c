8337c16954049414eca721163ff476f68ede1fc1
Thomas BERNARD <miniupnp@free.fr>
2013-03-19 00:22:57 +0100

    -Fix: Make Font loading/processing work for both Big & little endian

--- src/gui/font.c
+++ src/gui/font.c
@@ -6,6 +6,7 @@
 #include "types.h"
 
 #include "font.h"
+#include "../os/endian.h"
 
 #include "../config.h"
 #include "../file.h"
@@ -57,23 +58,31 @@ uint16 Font_GetStringWidth(char *string)
  */
 static Font *Font_LoadFile(const char *filename)
 {
-	uint16 *buf;
+	uint8 *buf;
 	Font *f;
 	uint8 i;
+	uint16 start;
+	uint16 dataStart;
+	uint16 widthList;
+	uint16 lineList;
 
 	if (!File_Exists(filename)) return NULL;
 
-	buf = (uint16 *)File_ReadWholeFile(filename);
+	buf = (uint8 *)File_ReadWholeFile(filename);
 
-	if (buf[1] != 0x0500) {
+	if (buf[2] != 0x00 || buf[3] != 0x05) {
 		free(buf);
 		return NULL;
 	}
 
 	f = (Font *)calloc(1, sizeof(Font));
-	f->height = ((uint8 *)buf)[buf[2] + 4];
-	f->maxWidth = ((uint8 *)buf)[buf[2] + 5];
-	f->count = buf[5] - buf[4];
+	start = READ_LE_UINT16(buf + 4);
+	dataStart = READ_LE_UINT16(buf + 6);
+	widthList = READ_LE_UINT16(buf + 8);
+	lineList = READ_LE_UINT16(buf + 12);
+	f->height = buf[start + 4];
+	f->maxWidth = buf[start + 5];
+	f->count = READ_LE_UINT16(buf + 10) - widthList;
 	f->chars = (FontChar *)calloc(f->count, sizeof(FontChar));
 
 	for (i = 0; i < f->count; i++) {
@@ -82,18 +91,18 @@ static Font *Font_LoadFile(const char *filename)
 		uint8 x;
 		uint8 y;
 
-		fc->width = ((uint8 *)buf)[buf[4] + i];
-		fc->unusedLines = ((uint8 *)buf)[buf[6] + i * 2];
-		fc->usedLines = ((uint8 *)buf)[buf[6] + i * 2 + 1];
+		fc->width = buf[widthList + i];
+		fc->unusedLines = buf[lineList + i * 2];
+		fc->usedLines = buf[lineList + i * 2 + 1];
 
-		dataOffset = buf[buf[3] / 2 + i];
+		dataOffset = READ_LE_UINT16(buf + dataStart + i * 2);
 		if (dataOffset == 0) continue;
 
 		fc->data = (uint8 *)malloc(fc->usedLines * fc->width);
 
 		for (y = 0; y < fc->usedLines; y++) {
 			for (x = 0; x < fc->width; x++) {
-				uint8 data = ((uint8 *)buf + dataOffset)[y * ((fc->width + 1) / 2) + x / 2];
+				uint8 data = buf[dataOffset + y * ((fc->width + 1) / 2) + x / 2];
 				if (x % 2 != 0) data >>= 4;
 				fc->data[y * fc->width + x] = data & 0xF;
 			}
