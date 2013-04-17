5d7fe00281474701afa9919a03d42ea2635f3388
Thomas BERNARD <miniupnp@free.fr>
2013-04-17 22:33:55 +0200

    -Fix: Follow coding style and use s_something for static variables
    Also improved comments

--- src/audio/midi_osx.c
+++ src/audio/midi_osx.c
@@ -1,10 +1,10 @@
-/** @file src/audio/midi_osx.c See PlaySoftMIDI example from the OS X SDK
- */
+/** @file src/audio/midi_osx.c Mac OS X implementation of the MIDI.
+ * See PlaySoftMIDI example from the OS X SDK. */
 
 #include "types.h"
 #include <AudioUnit/AudioUnit.h>
 #include <AudioToolbox/AudioToolbox.h>
-/*#include "midi.h"*/
+#include "midi.h"
 
 #if defined(AUDIO_TOOLBOX_VERSION) && (AUDIO_TOOLBOX_VERSION >= 0x0105)
 /* Use the new functions in favor of deprecated ones from OS X 10.5 onward */
@@ -13,8 +13,8 @@
 #define AUGraphGetNodeInfo(graph, node, desc, size, data, unit) AUGraphNodeInfo(graph, node, desc, unit)
 #endif
 
-static AUGraph graph = 0;
-static AudioUnit synthUnit;
+static AUGraph s_graph = 0;
+static AudioUnit s_synthUnit;
 
 /* This call creates the Graph and the Synth unit... */
 OSStatus CreateAUGraph()
@@ -28,34 +28,34 @@ OSStatus CreateAUGraph()
 	cd.componentFlags = 0;
 	cd.componentFlagsMask = 0;
 
-	result = NewAUGraph(&graph);
+	result = NewAUGraph(&s_graph);
 	if (result != 0) return result;
 
 	cd.componentType = kAudioUnitType_MusicDevice;
 	cd.componentSubType = kAudioUnitSubType_DLSSynth;
-	result = AUGraphNewNode(graph, &cd, 0, NULL, &synthNode);
+	result = AUGraphNewNode(s_graph, &cd, 0, NULL, &synthNode);
 	if (result != 0) return result;
 
 	cd.componentType = kAudioUnitType_Effect;
 	cd.componentSubType = kAudioUnitSubType_PeakLimiter;
-	result = AUGraphNewNode(graph, &cd, 0, NULL, &limiterNode);
+	result = AUGraphNewNode(s_graph, &cd, 0, NULL, &limiterNode);
 	if (result != 0) return result;
 
 	cd.componentType = kAudioUnitType_Output;
 	cd.componentSubType = kAudioUnitSubType_DefaultOutput;
-	result = AUGraphNewNode(graph, &cd, 0, NULL, &outNode);
+	result = AUGraphNewNode(s_graph, &cd, 0, NULL, &outNode);
 	if (result != 0) return result;
 
-	result = AUGraphOpen(graph);
+	result = AUGraphOpen(s_graph);
 	if (result != 0) return result;
 
-	result = AUGraphConnectNodeInput(graph, synthNode, 0, limiterNode, 0);
+	result = AUGraphConnectNodeInput(s_graph, synthNode, 0, limiterNode, 0);
 	if (result != 0) return result;
-	result = AUGraphConnectNodeInput(graph, limiterNode, 0, outNode, 0);
+	result = AUGraphConnectNodeInput(s_graph, limiterNode, 0, outNode, 0);
 	if (result != 0) return result;
 
 	/* Ok we're good to go - get the Synth Unit... */
-	result = AUGraphGetNodeInfo(graph, synthNode, 0, 0, 0, &synthUnit);
+	result = AUGraphGetNodeInfo(s_graph, synthNode, 0, 0, 0, &s_synthUnit);
 	return result;
 }
 
@@ -64,35 +64,37 @@ bool midi_init(void)
 	OSStatus result;
 	result = CreateAUGraph();
 	if (result != 0) return false;
-	result = AUGraphInitialize(graph);
+	result = AUGraphInitialize(s_graph);
 	if (result != 0) return false;
-	result = AUGraphStart(graph);
+	result = AUGraphStart(s_graph);
 	if (result != 0) return false;
 	return true;
 }
 
 void midi_uninit(void)
 {
-	if (graph == 0) return;
+	if (s_graph == 0) return;
 
-	AUGraphStop(graph);
-	DisposeAUGraph(graph);
-	graph = 0;
+	AUGraphStop(s_graph);
+	DisposeAUGraph(s_graph);
+	s_graph = 0;
 }
 
 /**
- * data = status | (data1 << 8) | (data2 << 16) */
+ * Sends a midi command
+ *
+ * @param data The data to send in "packed" format, ie status | (data1 << 8) | (data2 << 16) */
 void midi_send(uint32 data)
 {
 	OSStatus result;
-	result = MusicDeviceMIDIEvent(synthUnit, data & 0xff, (data >> 8) & 0xff, (data >> 16) & 0xff, 0);
+	result = MusicDeviceMIDIEvent(s_synthUnit, data & 0xff, (data >> 8) & 0xff, (data >> 16) & 0xff, 0);
 }
 
 void midi_reset(void)
 {
-	if (graph == 0) return;
+	if (s_graph == 0) return;
 
-	AUGraphStop(graph);
-	AUGraphStart(graph);
+	AUGraphStop(s_graph);
+	AUGraphStart(s_graph);
 }
 
