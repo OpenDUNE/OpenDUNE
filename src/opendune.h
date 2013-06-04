c377a423b65ff00b71c98b423bdb8cea747000d1
David Wang <dswang@users.sourceforge.net>
2013-06-05 07:27:22 +1000

    -Cleanup: remove prototype for Main() which no longer exists

--- src/opendune.h
+++ src/opendune.h
@@ -42,7 +42,6 @@ extern int16 g_musicInBattle;
 extern void *g_readBuffer;
 extern uint32 g_readBufferSize;
 
-extern void Main(void);
 extern void Game_Prepare(void);
 extern void Game_Init(void);
 extern void Game_LoadScenario(uint8 houseID, uint16 scenarioID);
