OpenDUNE/OpenDUNE

commit 0130d0270b0581e9e6aac82b449ec94e51cf0061

-CodeChange: renamed loc0A variable in GUI_Widget_Savegame_Click()
also added a few comments

src/gui/widget_click.c
@@ -852,7 +852,6 @@ static bool GUI_Widget_Savegame_Click(uint16 key)
 	bool loop;
 	char *saveDesc = g_savegameDesc[key];
 	bool widgetPaint;
-	uint16 loc0A;
 	bool ret;
 
 	if (*saveDesc == '[') *saveDesc = 0;
@@ -875,19 +874,20 @@ static bool GUI_Widget_Savegame_Click(uint16 key)
 	GUI_Mouse_Show_Safe();
 
 	for (loop = true; loop; sleepIdle()) {
+		uint16 key;
 		Widget *w = g_widgetLinkedListTail;
 
 		GUI_DrawText_Wrapper(NULL, 0, 0, 232, 235, 0x22);
 
-		loc0A = GUI_EditBox(saveDesc, 50, 15, g_widgetLinkedListTail, NULL, widgetPaint);
+		key = GUI_EditBox(saveDesc, 50, 15, g_widgetLinkedListTail, NULL, widgetPaint);
 		widgetPaint = false;
 
-		if ((loc0A & 0x8000) == 0) continue;
+		if ((key & 0x8000) == 0) continue;
 
-		GUI_Widget_MakeNormal(GUI_Widget_Get_ByIndex(w, loc0A & 0x7FFF), false);
+		GUI_Widget_MakeNormal(GUI_Widget_Get_ByIndex(w, key & 0x7FFF), false);
 
-		switch (loc0A & 0x7FFF) {
-			case 0x1E:
+		switch (key & 0x7FFF) {
+			case 0x1E:	/* RETURN / Save Button */
 				if (*saveDesc == 0) break;
 
 				SaveFile(GenerateSavegameFilename(s_savegameIndexBase - key), saveDesc);
@@ -895,7 +895,7 @@ static bool GUI_Widget_Savegame_Click(uint16 key)
 				ret = true;
 				break;
 
-			case 0x1F:
+			case 0x1F:	/* ESCAPE / Cancel Button */
 				loop = false;
 				ret = false;
 				FillSavegameDesc(true);

src/gui/editbox.c
@@ -47,7 +47,7 @@ static void GUI_EditBox_BlinkCursor(uint16 positionX, bool resetBlink)
  * @param w The widget this editbox is attached to (for input events).
  * @param tickProc The function to call every tick, for animation etc.
  * @param paint Flag indicating if the widget need to be repainted.
- * @return Unknown.
+ * @return Key code / Button press code.
  */
 uint16 GUI_EditBox(char *text, uint16 maxLength, uint16 widgetID, Widget *w, uint16 (*tickProc)(void), bool paint)
 {

commit afbf95a6a06da1b4a41c600a095ace6c8a2c12bb

-Fix: GUI_Widget_Savegame_Click() renamed key var and arg
fixes bug introduced by commit 0130d0270b0581e9e6aac82b449ec94e51cf0061

src/gui/widget_click.c
@@ -843,14 +843,14 @@ static void FillSavegameDesc(bool save)
 /**
  * Handles Click event for savegame button.
  *
- * @param key The index of the clicked button.
+ * @param index The index of the clicked button.
  * @return True if a game has been saved, False otherwise.
  */
-static bool GUI_Widget_Savegame_Click(uint16 key)
+static bool GUI_Widget_Savegame_Click(uint16 index)
 {
 	WindowDesc *desc = &g_savegameNameWindowDesc;
 	bool loop;
-	char *saveDesc = g_savegameDesc[key];
+	char *saveDesc = g_savegameDesc[index];
 	bool widgetPaint;
 	bool ret;
 
@@ -863,7 +863,7 @@ static bool GUI_Widget_Savegame_Click(uint16 key)
 	ret = false;
 	widgetPaint = true;
 
-	if (*saveDesc == '[') key = s_savegameCountOnDisk;
+	if (*saveDesc == '[') index = s_savegameCountOnDisk;
 
 	GFX_Screen_SetActive(SCREEN_0);
 
@@ -874,23 +874,23 @@ static bool GUI_Widget_Savegame_Click(uint16 key)
 	GUI_Mouse_Show_Safe();
 
 	for (loop = true; loop; sleepIdle()) {
-		uint16 key;
+		uint16 eventKey;
 		Widget *w = g_widgetLinkedListTail;
 
 		GUI_DrawText_Wrapper(NULL, 0, 0, 232, 235, 0x22);
 
-		key = GUI_EditBox(saveDesc, 50, 15, g_widgetLinkedListTail, NULL, widgetPaint);
+		eventKey = GUI_EditBox(saveDesc, 50, 15, g_widgetLinkedListTail, NULL, widgetPaint);
 		widgetPaint = false;
 
-		if ((key & 0x8000) == 0) continue;
+		if ((eventKey & 0x8000) == 0) continue;
 
-		GUI_Widget_MakeNormal(GUI_Widget_Get_ByIndex(w, key & 0x7FFF), false);
+		GUI_Widget_MakeNormal(GUI_Widget_Get_ByIndex(w, eventKey & 0x7FFF), false);
 
-		switch (key & 0x7FFF) {
+		switch (eventKey & 0x7FFF) {
 			case 0x1E:	/* RETURN / Save Button */
 				if (*saveDesc == 0) break;
 
-				SaveFile(GenerateSavegameFilename(s_savegameIndexBase - key), saveDesc);
+				SaveFile(GenerateSavegameFilename(s_savegameIndexBase - index), saveDesc);
 				loop = false;
 				ret = true;
 				break;