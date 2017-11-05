OpenDUNE/OpenDUNE

commit 04a17dfc3db3a655589b284069ab52ccbcebff5c

-Add: Named and removed 0A94 and 0A96 variables of input segment.

src/input/input.c
@@ -281,6 +281,7 @@ void Input_HandleInput(uint16 input)
 
 	uint16 inputMouseX;
 	uint16 inputMouseY;
+	uint16 tempBuffer[2];
 
 	s_input_local->flags = g_global->inputFlags;
 	inputMouseX = g_mouseX;
@@ -405,10 +406,9 @@ void Input_HandleInput(uint16 input)
 
 	if (g_global->mouseMode != INPUT_MOUSE_MODE_RECORD || value == 0x7D) return;
 
-	s_input_local->variable_0A94 = input;
-	s_input_local->variable_0A96 = g_timerInput;
-
-	File_Write(g_global->mouseFileID, &s_input_local->variable_0A94, saveSize);
+	tempBuffer[0] = input;
+	tempBuffer[1] = g_timerInput;
+	File_Write(g_global->mouseFileID, tempBuffer, saveSize);
 	g_timerInput = 0;
 }
 

src/input/input.h
@@ -86,8 +86,8 @@ typedef struct InputLocalData {
 	/* 074D()    */ PACK uint8   unknown_074D[0x02CC];
 
 	/* 0A19(123) */ PACK uint8      code_0A19[123];
-	/* 0A94(2)   */ PACK uint16 variable_0A94;              /*!< ?? */
-	/* 0A96(2)   */ PACK uint16 variable_0A96;              /*!< ?? Set to the same as 353F:76A6. */
+	/* 0A94(2)   */ PACK uint16  removed_0A94;              /*!< REMOVED - temporary buffer for writing input events to disk */
+	/* 0A96(2)   */ PACK uint16  removed_0A96;              /*!< REMOVED - temporary buffer for writing input events to disk */
 	/* 0A98(2)   */ PACK uint16 removed_mouseX;             /*!< REMOVED - Current X position of the mouse. */
 	/* 0A9A(2)   */ PACK uint16 removed_mouseY;             /*!< REMOVED - Current Y position of the mouse. */
 

commit cb851078345e6c6a1be6e962f76142c086517ac6

-Fix: fix DUNE.LOG input record/replay
Some bug had been introduced with 04a17dfc3db3a655589b284069ab52ccbcebff5c
and fc43202328532c52ec94a5ba41359082f962d856
The mouse (X, Y) position was not written to file (but 4 bytes of crap instead)

src/input/input.c
@@ -5,6 +5,7 @@
 #include "types.h"
 #include "../os/common.h"
 #include "../os/sleep.h"
+#include "../os/error.h"
 
 #include "input.h"
 
@@ -237,7 +238,11 @@ static void Input_ReadInputFromFile(void)
 
 	if (g_mouseMode == INPUT_MOUSE_MODE_NORMAL || g_mouseMode != INPUT_MOUSE_MODE_PLAY) return;
 
-	File_Read(g_mouseFileID, mouseBuffer, 4); /* Read failure not translated. */
+	if (File_Read(g_mouseFileID, mouseBuffer, 4) != 4) {
+		Warning("Input_ReadInputFromFile(): File_Read() error.\n");
+		return;
+	}
+	Debug("  time=%hu value=0x%04hx\n", mouseBuffer[1], mouseBuffer[0]);
 
 	g_mouseRecordedTimer = mouseBuffer[1];
 	value = g_mouseInputValue = mouseBuffer[0];
@@ -263,7 +268,11 @@ static void Input_ReadInputFromFile(void)
 		}
 	}
 
-	File_Read(g_mouseFileID, mouseBuffer, 4); /* Read failure not translated. */
+	if (File_Read(g_mouseFileID, mouseBuffer, 4) != 4) {
+		Warning("Input_ReadInputFromFile(): File_Read() error.\n");
+		return;
+	}
+	Debug("  mouseX=%hu mouseY=%hu\n", mouseBuffer[0], mouseBuffer[1]);
 
 	g_mouseX = g_mouseRecordedX = mouseBuffer[0];
 	value = g_mouseY = g_mouseRecordedY = mouseBuffer[1];
@@ -311,7 +320,7 @@ void Input_HandleInput(uint16 input)
 
 	uint16 inputMouseX;
 	uint16 inputMouseY;
-	uint16 tempBuffer[2];
+	uint16 tempBuffer[4];
 	uint16 flags; /* Mask for allowed input types. See InputFlagsEnum. */
 
 	flags       = g_inputFlags;
@@ -407,17 +416,14 @@ void Input_HandleInput(uint16 input)
 		/* mouse buttons : 0x2D '-' : no change
 		                   0x41 'A' : change for 1st button
 						   0x42 'B' : change for 2nd button */
-		if (Input_History_Add(inputMouseX) != 0) {
+		if ((Input_History_Add(inputMouseX) != 0) || (Input_History_Add(inputMouseY) != 0)) {
 			s_historyTail = oldTail;
 			return;
 		}
-		saveSize += 2;
 
-		if (Input_History_Add(inputMouseY) != 0) {
-			s_historyTail = oldTail;
-			return;
-		}
-		saveSize += 2;
+		tempBuffer[2] = inputMouseX;
+		tempBuffer[3] = inputMouseY;
+		saveSize += 4;
 	}
 
 	bit_value = 1;